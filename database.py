b8ad54d9f9f9d989fdf73b7bcc07db0c7b611521
AndreasOehlenschlaeger <andreasoeh@outlook.dk>
2026-06-02 19:52:31 +0200

    typo

--- database.py
+++ database.py
@@ -4,8 +4,8 @@ import csv
 
 # Try to get from system enviroment variable
 # Set your Postgres user and password as second arguments of these two next function calls
-user = os.environ.get('PGUSER', 'andr')
-password = os.environ.get('PGPASSWORD', '1234')
+user = os.environ.get('PGUSER', 'tingr')
+password = os.environ.get('PGPASSWORD', '123')
 host = os.environ.get('HOST', '127.0.0.1')
 
 
